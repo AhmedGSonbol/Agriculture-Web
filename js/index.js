68cb747db8f6b6bcadc19b2ee35b2ed4be3612a3
Ahmed Gehad Sonbol <126677774+AhmedGSonbol@users.noreply.github.com>
2023-09-05 22:56:21 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -130,18 +130,18 @@ var swiper = new Swiper(".slide-content", {
 
 
 
-  let btn = document.getElementById('indecator2');
-  window.onscroll = function(){
-    if(scrollY >= 100){
-    btn.style.display = 'block';
-    } else {
-    btn.style.display = 'none';
-    }
-  }
-  btn.onclick = function(){
-    scroll({
-    left: 0,
-    top: 0,
-    behavior: "smooth"
-    })
-  }
+  // let btn = document.getElementById('indecator2');
+  // window.onscroll = function(){
+  //   if(scrollY >= 100){
+  //   btn.style.display = 'block';
+  //   } else {
+  //   btn.style.display = 'none';
+  //   }
+  // }
+  // btn.onclick = function(){
+  //   scroll({
+  //   left: 0,
+  //   top: 0,
+  //   behavior: "smooth"
+  //   })
+  // }
